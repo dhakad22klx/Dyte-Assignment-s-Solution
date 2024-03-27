30b8bb807515c37de8789838e7f786d59325672a
dhakad22klx <dhakad22klx.pvt@gmail.com>
2024-03-27 23:09:40 +0530

    first

---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Nothing here.
+	// Run the following command in CLI:
+	// go test -benchmem -bench BenchmarkConnections
+
+	fmt.Println("Running Fine");
+}
