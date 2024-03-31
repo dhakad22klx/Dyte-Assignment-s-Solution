e4f7c1bd061157d02363a103dfd22a49d2292856
dhakad22klx <dhakad22klx.pvt@gmail.com>
2024-04-01 00:10:29 +0530

    Resubmission

--- benchs_test.go
+++ benchs_test.go
@@ -183,7 +183,7 @@ func BenchmarkRawUDP(b *testing.B) {
 // Please read the comments carefully. You need to implement something atleast much faster than the baseline
 func BenchmarkSample(b *testing.B) {
 	// Stop the timer to perform setup operations
-    b.StopTimer()
+	b.StopTimer()
 
 	testPort := 40101
 	// Create a UDP connection to send messages.
@@ -196,61 +196,61 @@ func BenchmarkSample(b *testing.B) {
 	}
 	defer conn.Close()
 
-    // Preallocate UDP addresses for each port.
+	// Preallocate UDP addresses for each port.
 	// These addresses will be used to specify the destination for each message.
-    ports, readChan, closeChan, err := testInit(readersCount, false)
-    if err != nil {
-        b.Fatal(err)
-    }
-    defer close(closeChan)
+	ports, readChan, closeChan, err := testInit(readersCount, false)
+	if err != nil {
+		b.Fatal(err)
+	}
+	defer close(closeChan)
 
-    // Create a connection pool : Precomputation
-    connPool := make([]*net.UDPConn, readersCount)
-    for i, port := range ports {
-        conn, err := net.DialUDP("udp", nil, &net.UDPAddr{
-            IP:   net.IPv4(127, 0, 0, 1),
-            Port: port,
-        })
-        if err != nil {
-            b.Fatal(err)
-        }
-        defer conn.Close()
-        connPool[i] = conn
-    }
+	// Create a connection pool : Precomputation
+	connPool := make([]*net.UDPConn, readersCount)
+	for i, port := range ports {
+		conn, err := net.DialUDP("udp", nil, &net.UDPAddr{
+			IP:   net.IPv4(127, 0, 0, 1),
+			Port: port,
+		})
+		if err != nil {
+			b.Fatal(err)
+		}
+		defer conn.Close()
+		connPool[i] = conn
+	}
 
 	// writer is a function responsible for sending messages to all readers.
-    writer := func() {
-        var wg sync.WaitGroup
-        wg.Add(readersCount)
+	writer := func() {
+		var wg sync.WaitGroup
+		wg.Add(readersCount)
 
-        // Concurrently write to connections
-        for _, conn := range connPool {
-			conn := conn                   // to make unique otherwise will be shared with each goroutines 
-            go func(){
-                defer wg.Done()
-                buf := getTestMsg()
-                _, err := conn.Write(buf)  // Send the message to the corresponding reader.
-                if err != nil {
-                    b.Fatal(err)
-                }
-            }()
-        }
+		// Concurrently write to connections
+		for _, conn := range connPool {
+			conn := conn // to make unique otherwise will be shared with each goroutines
+			go func() {
+				defer wg.Done()
+				buf := getTestMsg()
+				_, err := conn.Write(buf) // Send the message to the corresponding reader.
+				if err != nil {
+					b.Fatal(err)
+				}
+			}()
+		}
 
-        // Wait for all write operations to complete
-        wg.Wait()
+		// Wait for all write operations to complete
+		wg.Wait()
 
-        // Wait for readers
-        waitForReaders(readChan, b) 
-    }
+		// Wait for readers
+		waitForReaders(readChan, b)
+	}
 
 	// Start the timer to measure the benchmark.
-    b.StartTimer()
+	b.StartTimer()
 
 	// Execute the benchmark for the specified number of iterations.
-    for i := 0; i < b.N; i++ {
-        writer()
-    }
+	for i := 0; i < b.N; i++ {
+		writer()
+	}
 
 	// Stop the timer after completing the benchmark iterations.
-    b.StopTimer()
+	b.StopTimer()
 }
